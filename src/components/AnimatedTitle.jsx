4f39a123de93c6ac92ae29b89f22e9c78f626ab6
DipakVyavahre <vyavahared7@gmail.com>
2025-12-14 21:39:06 +0530

    hero about navbar implemented

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,14 +1,13 @@
-import { gsap } from "gsap";
-import { useEffect, useRef } from "react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import clsx from "clsx";
-
+import React, { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import clsx from 'clsx';
 gsap.registerPlugin(ScrollTrigger);
 
-const AnimatedTitle = ({ title, containerClass }) => {
-  const containerRef = useRef(null);
+const AnimatedTitle = ({title , containerClass}) => {
+  const containerRef = useRef(null) ;
 
-  useEffect(() => {
+ useEffect(() => {
     const ctx = gsap.context(() => {
       const titleAnimation = gsap.timeline({
         scrollTrigger: {
@@ -35,23 +34,16 @@ const AnimatedTitle = ({ title, containerClass }) => {
   }, []);
 
   return (
-    <div ref={containerRef} className={clsx("animated-title", containerClass)}>
-      {title.split("<br />").map((line, index) => (
-        <div
-          key={index}
-          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
-        >
-          {line.split(" ").map((word, idx) => (
-            <span
-              key={idx}
-              className="animated-word"
-              dangerouslySetInnerHTML={{ __html: word }}
-            />
-          ))}
-        </div>
-      ))}
+    <div ref={containerRef} className={ `animated-title ${containerClass  }`}>
+        {title.split('<br/>').map((line, index) => (
+          <div key={index} className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3">
+            {line.split(' ').map((word, i) => (
+              <span key={i} className="animated-word" dangerouslySetInnerHTML={{__html: word}}/>
+            ))}
+            </div>
+        ))}        
     </div>
-  );
-};
+  )
+}
 
-export default AnimatedTitle;
+export default AnimatedTitle
