2dc9bd7a912acb30f606f7ca17d453ec6bce8937
DipakVyavahre <vyavahared7@gmail.com>
2025-12-15 23:14:53 +0530

    feature component inplemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import About from "./components/About";
+import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />   
       <About />
+      <Features/>
     </main>
   );
 }
